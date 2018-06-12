e0637dd99ec3086e6b2b566fec30e38b80226fb7
Antonio Jesus Ríos Cabrera <antonio.rios.cabrera@avanade.com>
2018-06-12 11:02:58 +0200

    Angular Course: Module 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ProductService} from './products/product.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template:`
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+  `,
+  providers: [ProductService]
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle: string = "Acme Product Management";
 }
